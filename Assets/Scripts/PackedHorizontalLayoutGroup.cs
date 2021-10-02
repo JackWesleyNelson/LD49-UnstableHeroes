8ba527d10fe417b1d4f03331ed6fbe4dcdae1dc1
Jack W Nelson <nelsonw948@msn.com>
2021-10-01 19:58:23 -0600

    initial commit

---
+++ Assets/Scripts/PackedHorizontalLayoutGroup.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//TODO: Handle if elements are wider than the layout group
+public class PackedHorizontalLayoutGroup : HorizontalLayoutGroup
+{
+    void Update()
+    {
+        RectTransform layoutRect = GetComponent<RectTransform>();
+        if (layoutRect != null)
+        {
+            float widthOfChildren = 0;
+            foreach (Transform child in transform)
+            {
+                RectTransform r = child.GetComponent<RectTransform>();
+                if (r != null)
+                {
+                    widthOfChildren += r.rect.width;
+                }
+            }
+            spacing = -layoutRect.rect.width + widthOfChildren;
+        }
+    }
+}
---
+++ Assets/Scripts/PackedVerticalLayoutGroup.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//TODO: Handle if the elements are taller than the layout group.
+public class PackedVerticalLayoutGroup : VerticalLayoutGroup
+{
+    void Update()
+    {
+        RectTransform layoutRect = GetComponent<RectTransform>();
+        if (layoutRect != null)
+        {
+            float heightOfChildren = 0;
+            foreach (Transform child in transform)
+            {
+                RectTransform r = child.GetComponent<RectTransform>();
+                if (r != null)
+                {
+                    heightOfChildren += r.rect.height;
+                }
+            }
+            spacing = -layoutRect.rect.height + heightOfChildren;
+        }
+    }
+}
